c7619bc368db89017b22d9f792cdd8a3a34bc428
DESKTOP-BLJANOR\Leon <LeonLee@potix.com>
2018-07-03 16:17:02 +0800

    修正

--- test1workspace/js/week1_test1.js
+++ test1workspace/js/week1_test1.js
@@ -1,17 +1,17 @@
 var timer;
 var target = 0;
-var scrollDiv = document.getElementById("scrollDiv");
+var scrollDiv = document.getElementById('scrollDiv');
 
-document.addEventListener("DOMContentLoaded", function (event) {
-    document.getElementById("leftButton").addEventListener("click", scroll);
-    document.getElementById("rightButton").addEventListener("click", scroll);
+document.addEventListener('DOMContentLoaded', function (event) {
+    document.getElementById('leftButton').addEventListener('click', scroll);
+    document.getElementById('rightButton').addEventListener('click', scroll);
 });
 
 function scroll() {
     if (timer) {
         clearInterval(timer);
     }
-    if (this.id == "leftButton") {
+    if (this.id == 'leftButton') {
         if (target >= 200) {
             target -= 200;
         }
@@ -20,7 +20,7 @@ function scroll() {
                 if (scrollDiv.scrollLeft > target) { scrollDiv.scrollLeft -= 4; } else { clearInterval(timer); }
             }, 10);
         }
-    } else if (this.id == "rightButton") {
+    } else if (this.id == 'rightButton') {
         if (target <= 200) {
             target += 200;
         }
