7f47d9cd9d50282650aa3db310c2d8604c40e929
DESKTOP-BLJANOR\Leon <LeonLee@potix.com>
2018-07-05 11:26:37 +0800

    完成1

--- week1-2/js/puzzle.js
+++ week1-2/js/puzzle.js
@@ -1,54 +1,50 @@
 var dragSource = document.getElementsByClassName('img');
-var startDiv;
-var endDivClass;
-var startMPos;
-var green;
+var align = [document.getElementById('1').offsetLeft, document.getElementById('1').offsetTop]; //置中位移量
+var endDivClass; //交換
+var startDiv; //起始元素
+var startMPos; //起始鼠標
+var startRMP; //起始鼠標相對起始元素內座標
+var green = document.getElementsByClassName('green')[0]; //綠框
 
 document.addEventListener('DOMContentLoaded', function (event) {
     for (var i = 0; i <= 8; i++) {
         dragSource[i].addEventListener('mousedown', dragStart);
-        dragSource[i].addEventListener('mouseup', dropped,true)
+        dragSource[i].addEventListener('mouseup', dropped)
     }
 });
 
 function dragStart(e) {
     pauseEvent(e);
-    var sq = document.createElement('div');
-    this.appendChild(sq);
-    // this.children[0].style.className = 'green';
-    green = this.children[0];
     startDiv = this;
     startMPos = [e.clientX, e.clientY];
-    green.className = 'img green';
+    startRMP = [e.clientX - this.offsetLeft, e.clientY - this.offsetTop];
     green.style.left = this.offsetLeft + 'px';
     green.style.top = this.offsetTop + 'px';
     green.style.visibility = 'visible';
-    for (var i = 0; i <= 8; i++) {
-        dragSource[i].addEventListener('mousemove', move);
-    }
-    console.log(this.children[0])
-    green.addEventListener('mouseup', www,true)
+    document.addEventListener('mousemove', move);
+    // console.log(this)
+    green.addEventListener('mouseup', dropped)
 }
 
 function dropped(e) {
     pauseEvent(e);
     var endMPos = [e.clientX, e.clientY];
-    if (Math.abs(startDiv.offsetLeft - this.offsetLeft) + Math.abs(startDiv.offsetTop - this.offsetTop) == 150) { //判斷相鄰
-        if (Math.abs(endMPos[0] - startMPos[0]) >= 75 || Math.abs(endMPos[1] - startMPos[1]) >= 75) { //判斷過半
-            change(this);
+    if (endMPos[0] > align[0] && endMPos[0] < align[0] + 450 && endMPos[1] > align[1] && endMPos[1] < align[1] + 450) { //判斷是否在拼圖內
+        var endDivId = (Math.floor((endMPos[0] - align[0]) / 150) + 1) + Math.floor((endMPos[1] - align[1]) / 150) * 3;
+        var endDiv = document.getElementById(endDivId);
+        if (Math.abs(startDiv.offsetLeft - endDiv.offsetLeft) + Math.abs(startDiv.offsetTop - endDiv.offsetTop) == 150) { //判斷相鄰
+            if (Math.abs(endMPos[0] - startMPos[0]) >= 75 || Math.abs(endMPos[1] - startMPos[1]) >= 75) { //判斷過半
+                change(endDiv);
+            }
         }
     }
-    for (var i = 0; i <= 8; i++) {
-        dragSource[i].removeEventListener('mousemove', move);
-    }
+    document.removeEventListener('mousemove', move);
     green.style.visibility = 'hidden';
 }
 
 function move(e) {
-    pauseEvent(e);
-    green.style.left = (e.clientX-50) + 'px';
-    green.style.top = (e.clientY-50) + 'px';
-    console.log(green.offsetLeft);
+    green.style.left = (e.clientX - startRMP[0]) + 'px';
+    green.style.top = (e.clientY - startRMP[1]) + 'px';
 }
 
 function change(end) {
@@ -60,8 +56,4 @@ function change(end) {
 function pauseEvent(e) {
     e.stopPropagation();
     e.preventDefault();
-}
-
-function www(){
-    console.log('www')
 }
