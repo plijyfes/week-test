7e2a4d9364e322d7d9c795013676dc1378cdbfb7
DESKTOP-BLJANOR\Leon <LeonLee@potix.com>
2018-07-05 18:29:20 +0800

    final

--- week1-2/js/puzzle.js
+++ week1-2/js/puzzle.js
@@ -1,16 +1,17 @@
-var dragSource = document.getElementsByClassName('img');
-var align = [document.getElementById('1').offsetLeft, document.getElementById('1').offsetTop]; //置中位移量
-var endDivClass; //交換
-var startDiv; //起始元素
-var startMPos; //起始鼠標
-var startRMP; //起始鼠標相對起始元素內座標
-var green = document.getElementsByClassName('green')[0]; //綠框
+var imgSize = document.getElementById('1').offsetWidth,
+    dragging, //拖曳中
+    startDiv, //起始元素
+    startMPos, //起始鼠標
+    startRMP, //起始鼠標相對起始元素內座標
+    green = document.getElementById('green'); //綠框
 
 document.addEventListener('DOMContentLoaded', function (event) {
-    for (var i = 0; i <= 8; i++) {
+    var dragSource = document.getElementById('blockContainer').children;
+    for (var i = 0; i < dragSource.length; i++) {
         dragSource[i].addEventListener('mousedown', dragStart);
-        dragSource[i].addEventListener('mouseup', dropped)
     }
+    document.addEventListener('mousemove', move);
+    green.addEventListener('mouseup', dropped);
 });
 
 function dragStart(e) {
@@ -18,39 +19,64 @@ function dragStart(e) {
     startDiv = this;
     startMPos = [e.clientX, e.clientY];
     startRMP = [e.clientX - this.offsetLeft, e.clientY - this.offsetTop];
+    dragging = true;
     green.style.left = this.offsetLeft + 'px';
     green.style.top = this.offsetTop + 'px';
     green.style.visibility = 'visible';
-    document.addEventListener('mousemove', move);
-    // console.log(this)
-    green.addEventListener('mouseup', dropped)
 }
 
 function dropped(e) {
     pauseEvent(e);
+    var blockContainer = document.getElementById('blockContainer');
     var endMPos = [e.clientX, e.clientY];
-    if (endMPos[0] > align[0] && endMPos[0] < align[0] + 450 && endMPos[1] > align[1] && endMPos[1] < align[1] + 450) { //判斷是否在拼圖內
-        var endDivId = (Math.floor((endMPos[0] - align[0]) / 150) + 1) + Math.floor((endMPos[1] - align[1]) / 150) * 3;
+    if (isInPuzzle(endMPos)) { //判斷是否在拼圖內
+        var endDivId = (Math.floor((endMPos[0] - blockContainer.offsetLeft) / imgSize) + 1) + Math.floor((endMPos[1] - blockContainer.offsetTop) / imgSize) * 3;
         var endDiv = document.getElementById(endDivId);
-        if (Math.abs(startDiv.offsetLeft - endDiv.offsetLeft) + Math.abs(startDiv.offsetTop - endDiv.offsetTop) == 150) { //判斷相鄰
-            if (Math.abs(endMPos[0] - startMPos[0]) >= 75 || Math.abs(endMPos[1] - startMPos[1]) >= 75) { //判斷過半
+        if (isNeighbour(endDiv)) { //判斷相鄰
+            if (isHalf(endMPos)) { //判斷過半
                 change(endDiv);
             }
         }
     }
-    document.removeEventListener('mousemove', move);
+    dragging = false;
     green.style.visibility = 'hidden';
 }
 
 function move(e) {
-    green.style.left = (e.clientX - startRMP[0]) + 'px';
-    green.style.top = (e.clientY - startRMP[1]) + 'px';
+    if (dragging) {
+        green.style.left = (e.clientX - startRMP[0]) + 'px';
+        green.style.top = (e.clientY - startRMP[1]) + 'px';
+    }
 }
 
 function change(end) {
-    endDivClass = end.className;
+    var endDivClass = end.className;
     end.className = startDiv.className;
     startDiv.className = endDivClass;
+}
+
+function isInPuzzle(endMPos) {
+    if (endMPos[0] > blockContainer.offsetLeft && endMPos[0] < blockContainer.offsetLeft + blockContainer.offsetWidth && endMPos[1] > blockContainer.offsetTop && endMPos[1] < blockContainer.offsetTop + blockContainer.offsetHeight) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isNeighbour(endDiv) {
+    if (Math.abs(startDiv.offsetLeft - endDiv.offsetLeft) + Math.abs(startDiv.offsetTop - endDiv.offsetTop) == imgSize) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isHalf(endMPos) {
+    if (Math.abs(endMPos[0] - startMPos[0]) >= 0.5 * imgSize || Math.abs(endMPos[1] - startMPos[1]) >= 0.5 * imgSize) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function pauseEvent(e) {
