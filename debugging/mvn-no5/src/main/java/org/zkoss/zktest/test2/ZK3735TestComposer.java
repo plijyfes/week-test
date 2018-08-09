e1ac182ecfd2ab541d91a6583a23c4c87558c52d
LEON-NB\rd <LeonLee@potix.com>
2018-08-09 12:19:40 +0800

    debugging

---
+++ debugging/mvn-no5/src/main/java/org/zkoss/zktest/test2/ZK3735TestComposer.java
@@ -0,0 +1,55 @@
+package org.zkoss.zktest.test2;
+
+import java.util.ArrayList;
+
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zk.ui.util.Clients;
+import org.zkoss.zul.ListModelList;
+import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Listitem;
+import org.zkoss.zul.Window;
+
+
+public class ZK3735TestComposer extends SelectorComposer<Window> {
+    @Wire
+    Listbox listbox;
+
+    @Listen("onClick=#set1")
+    public void setM1() {
+        ArrayList<String> list1 = new ArrayList<String>();
+        list1.add("item1");
+        ListModelList<String> listmodel1 = new ListModelList<String>(list1);
+        listbox.setModel(listmodel1);
+        print();
+    }
+
+    @Listen("onClick=#set2")
+    public void setM2() {
+        ArrayList<String> list2 = new ArrayList<String>();
+        list2.add("item1");
+        ListModelList<String> listmodel2 = new ListModelList<String>(list2);
+        listbox.setModel(listmodel2);
+        print();
+    }
+
+    @Listen("onClick=#set3")
+    public void setM3() {
+        ArrayList<String> list3 = new ArrayList<String>();
+        for (int i = 1; i <= 8; i++) {
+            list3.add("item" + i);
+        }
+        ListModelList<String> listmodel3 = new ListModelList<String>(list3);
+        listbox.setModel(listmodel3);
+        print();
+    }
+
+    public void print() {
+        Clients.log("listboxSize : " + listbox.getItemCount());
+        Clients.log("visibleItemCount : " + listbox.getVisibleItemCount());
+        for (Listitem item : listbox.getItems()) {
+            Clients.log(item + " - " + item.isVisible());
+        }
+    }
+}
